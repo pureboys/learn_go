4f96d95587ec25474e6e79f9c8b277d1660e2947
yuyuud <yuyuud@gmail.com>
2019-07-01 21:39:22 +0800

    day15

--- day14/SecKill/SecProxy/service/redis_proc.go
+++ day14/SecKill/SecProxy/service/redis_proc.go
@@ -35,7 +35,7 @@ func ReadHandle() {
 	for {
 
 		conn := secKillConf.proxy2LayerRedisPool.Get()
-		reply, err := conn.Do("RPOP", "recv_queue")
+		reply, err := conn.Do("BRPOP", "recv_queue", 1000)
 		data, err := redis.String(reply, err)
 		if err != nil {
 			logs.Error("rpop failed, err:%v", err)
